95ba5ce159b0c2dd493fba6b599a00ba033a0965
Will Drevo <will.drevo@gmail.com>
2014-04-27 16:15:20 -0400

    dht in place

---
+++ run.go
@@ -0,0 +1,9 @@
+package main
+
+import "dht"
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, Peerchat")
+	dht.MakeDHT()
+}
---
+++ src/dht/dht.go
@@ -0,0 +1,27 @@
+package dht
+
+type Peer struct {
+	NodeID int
+	Address int
+	Port int
+}
+
+type DHT struct {
+	
+}
+
+func (dht *DHT) Ping() {
+}
+
+func (dht *DHT) Store() {
+}
+
+func (dht *DHT) FindNode() {
+}
+
+func (dht *DHT) FindValue() {
+}
+
+func MakeDHT() {
+
+}
