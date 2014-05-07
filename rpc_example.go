624cef77da9754a3b853e07a5e2d8e041a18fd9b
Will Drevo <will.drevo@gmail.com>
2014-05-06 23:09:35 -0400

    Added chat example with async and stack examples too

--- rpc_example.go
+++ rpc_example.go
@@ -4,12 +4,17 @@ import "example"
 import "log"
 import "strconv"
 import "lang"
+import "fmt"
+import "bufio"
+import "os"
+import "time"
 
 func main() {	
 	
 	//sync()
 	//async()
-	stack_ex()
+	//stack_ex()
+	startChat()
 }
 
 func sync() {
@@ -58,4 +63,47 @@ func stack_ex() {
 	log.Printf(stack.Pop().(string))
 	log.Printf(stack.Pop().(string))
 	log.Printf(stack.Pop().(string))
+}
+
+func startChat() {
+	fmt.Printf("********************")
+	fmt.Printf(" WELCOME TO PEERCHAT! ")
+	fmt.Printf("********************\n")
+	
+	// input a friends IP address 
+	// TODO: logic for if you've already registered
+	fmt.Printf("Enter a friend's address (ip:port) to get started:\n")
+	in := bufio.NewReader(os.Stdin)
+    peerAddress, err := in.ReadString('\n')
+    peerAddress = peerAddress[:len(peerAddress)-1]
+    if err != nil {
+    	// handle error
+    }
+    
+    // so sexy
+    fmt.Printf("Connecting to %s", peerAddress)
+    time.Sleep(300 * time.Millisecond)
+    fmt.Printf(".")
+    time.Sleep(300 * time.Millisecond)
+    fmt.Printf(".")
+    time.Sleep(300 * time.Millisecond)
+    fmt.Printf(".\n")
+    
+    for {
+    	fmt.Printf("%s> ", peerAddress)
+    	input, _ := in.ReadString('\n')
+    	input = input[:len(input)-1]
+    	
+    	if input == "exit" {
+    		fmt.Printf("Signing out!\n")
+    		break
+    		
+    	} else if input[0] == 92 { // "\"
+    		fmt.Printf("Swtiching to talk to: %s\n", input[1:])
+    		peerAddress = input[1:]
+    		
+    	} else {
+    		fmt.Printf("Sending: \"%s\"...\n", input)
+    	}
+    }
 }
